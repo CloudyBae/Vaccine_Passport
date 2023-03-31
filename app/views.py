93de7f06cd35a8c6127ba128595aff23b38d4cef
Yoojin93 <yoojinbae93@gmail.com>
2023-03-30 19:23:14 -0500

    views.py

--- app/views.py
+++ app/views.py
@@ -1,13 +1,14 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 
 @app.route("/")
 def index():
-    return render_template("homepage.html")
+    return render_template("main.html")
 
-@app.route("/patients")
+@app.route("/patients", methods=["POST"])
 def list_of_patients():
-    return "List of patients"
+    email_address = request.form["email-address"]
+    return email_address
 
 @app.route("/patient")
 def individual_patient():
