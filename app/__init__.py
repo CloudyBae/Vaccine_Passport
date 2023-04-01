9092a88e56e36ffdecbeab966b34f2efd5bce43d
Yoojin93 <yoojinbae93@gmail.com>
2023-03-31 21:24:12 -0500

    updated init

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from app import views
+from app import models
 
 app = Flask(__name__, instance_relative_config=True)
 app.config["SECRET_KEY"] = '571ebf8e12ca209536c'
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///doctor_patient_information.db'
-app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
-app.config.from_object("config")
+app.config.from_object('config')
 
-from app import views
-from app import models
